6afa3f940d1bde7023eb6b7b83063477e1ebac28
Dean Cain <b01787017@studentmail.uws.ac.uk>
2026-02-19 11:13:51 +0000

    Refactor AreaCalculator to use Shape polymorphism (remove instanceof)

--- com/uws/excercise2/AreaCalculator.java
+++ com/uws/excercise2/AreaCalculator.java
@@ -1,16 +1,11 @@
 package com.uws.excercise2;
 
 public class AreaCalculator {
-    public static double calculateArea(Object shape) {
-        if (shape instanceof Rectangle) {
-            Rectangle rectangle = (Rectangle) shape;
-            return rectangle.getWidth() * rectangle.getHeight();
-        } 
-        else if (shape instanceof Circle) {
-            Circle circle = (Circle) shape;
-            return Math.PI * circle.getRadius() * circle.getRadius();
-        }
-        throw new IllegalArgumentException("Unsupported shape type");
-    }
 
+    public static double calculateArea(Shape shape) {
+        if (shape == null) {
+            throw new IllegalArgumentException("Shape must not be null");
+        }
+        return shape.area();
+    }
 }
