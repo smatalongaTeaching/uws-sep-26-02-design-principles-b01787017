9b80dceac0bea00b5752025bd75c043d303f7140
Dean Cain <b01787017@studentmail.uws.ac.uk>
2026-02-19 13:19:32 +0000

    Refactor: adapt EmailSender and SMSSender to NotificationChannel

--- com/uws/excercise5/EmailSender.java
+++ com/uws/excercise5/EmailSender.java
@@ -1,9 +1,14 @@
 package com.uws.excercise5;
 
-public class EmailSender {
+public class EmailSender implements NotificationChannel {
+
+    @Override
+    public void send(String recipient, String message) {
+        sendEmail(recipient, "Notification", message);
+    }
+
     public void sendEmail(String recipient, String subject, String message) {
         System.out.println("Sending email to " + recipient + " with subject: " + subject);
         // Email sending logic...
     }
-
 }
--- com/uws/excercise5/SMSSender.java
+++ com/uws/excercise5/SMSSender.java
@@ -1,9 +1,14 @@
 package com.uws.excercise5;
 
-public class SMSSender {
-        public void sendSMS(String phoneNumber, String message) {
+public class SMSSender implements NotificationChannel {
+
+    @Override
+    public void send(String recipient, String message) {
+        sendSMS(recipient, message);
+    }
+
+    public void sendSMS(String phoneNumber, String message) {
         System.out.println("Sending SMS to " + phoneNumber);
         // SMS sending logic...
     }
-
 }
