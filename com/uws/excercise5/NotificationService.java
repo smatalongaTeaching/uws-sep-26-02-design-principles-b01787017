d3073a1bf3ed2f0e32effb9a1c185f699a8fa856
Dean Cain <b01787017@studentmail.uws.ac.uk>
2026-02-19 13:20:21 +0000

    Refactor: inject NotificationChannel dependencies into NotificationService

--- com/uws/excercise5/NotificationService.java
+++ com/uws/excercise5/NotificationService.java
@@ -1,22 +1,20 @@
 package com.uws.excercise5;
 
 public class NotificationService {
-    private EmailSender emailSender;
-    private SMSSender smsSender;
-    
-    public NotificationService() {
-        this.emailSender = new EmailSender(); // Direct dependency on concrete class
-        this.smsSender = new SMSSender();     // Direct dependency on concrete class
-    }
-    
-    public void sendNotification(String user, String message) {
-        if (user.contains("@")) { //Covert type check
-            // Assume it's an email
-            emailSender.sendEmail(user, "Notification", message);
-        } else {
-            // Assume it's a phone number
-            smsSender.sendSMS(user, message);
-        }
+
+    private final NotificationChannel emailChannel;
+    private final NotificationChannel smsChannel;
+
+    public NotificationService(NotificationChannel emailChannel, NotificationChannel smsChannel) {
+        this.emailChannel = emailChannel;
+        this.smsChannel = smsChannel;
     }
 
+    public void sendNotification(String user, String message) {
+        if (user.contains("@")) { 
+            emailChannel.send(user, message);
+        } else {
+            smsChannel.send(user, message);
+        }
+    }
 }
